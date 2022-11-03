7b8e54e9245f6c231b5f5503dc9528478b3f0cd5
Akash Uthayan <AKASHU.20@ichat.sp.edu.sg>
2022-11-03 12:31:15 +0800

    This is the new version

---
+++ Ex 2.py
@@ -0,0 +1,8 @@
+def display_main_menu():
+    print("display_main_menu")
+def calc_average():
+    print("calc_average")
+
+if __name__ == "__main__":
+    display_main_menu()
+    calc_average()
--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def calculate_bmi(height, weight):
+    print("Height = "+str(height))
+    print("Weight = " + str(weight))
+    bmi= weight/(height*height)
+    print("BMI="+ str(round(bmi,2)))
+    if bmi<18.5 :
+        print("You are underweight")
+    elif bmi>=18.5 and bmi <=25.0:
+        print("Your weight is in the normal range")
+    else:
+        print("You are underweight")
+
+calculate_bmi(weight=57, height= 1.73)
